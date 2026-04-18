d17c7666d7942fe84bdf9ba9ca0413b1750fd13a
sene03 <hansel0311@gmail.com>
2026-04-18 10:46:20 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,4 +1,5 @@
-select a.ITEM_ID as ITEM_ID, a.ITEM_NAME as ITEM_NAME from 
-ITEM_INFO a join ITEM_TREE b
-on a.ITEM_ID = b.ITEM_ID
-where b.PARENT_ITEM_ID is null;
+select ITEM_ID, ITEM_NAME
+from ITEM_INFO
+where ITEM_ID in
+(select ITEM_ID from ITEM_TREE
+where PARENT_ITEM_ID is null);
