2db7daecff810e6bee7e3bf72f1d792ab953a335
sene03 <hansel0311@gmail.com>
2026-04-24 08:14:20 +0900

    [level 2] Title: 타겟 넘버, Time: 168.12 ms, Memory: 49.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,16 +1,9 @@
 def solution(numbers, target):
-    n = len(numbers)
-    ans = 0
-    
-    def dfs(idx, cur):
-        nonlocal ans
-        if (idx == n):
-            if (cur == target):
-                ans += 1
-            return 
-        
-        dfs(idx+1, cur + numbers[idx])
-        dfs(idx+1, cur - numbers[idx])
-    
-    dfs(0, 0)
-    return ans
+    leaves = [0]
+    for n in numbers:
+        tmp = []
+        for l in leaves:
+            tmp.append(l + n)
+            tmp.append(l - n)
+        leaves = tmp
+    return leaves.count(target)
