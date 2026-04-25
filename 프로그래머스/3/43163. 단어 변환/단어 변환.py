342d2304dd7749bdb6b21ab2fc9ff6ed902f565b
sene03 <hansel0311@gmail.com>
2026-04-25 13:07:19 +0900

    [level 3] Title: 단어 변환, Time: 7.17 ms, Memory: 9.16 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,28 @@
+from collections import deque
+def solution(begin, target, words):
+    
+    # 변환 가능한지 확인하는 함수
+    def can_transform(w1, w2):
+        diff = 0
+        for i in range(len(w1)):
+            if w1[i] != w2[i]:
+                diff += 1
+                
+        return diff == 1
+    
+    if target not in words:
+        return 0
+    
+    q = deque([(begin, 0)]) # word, depth
+    visited = [False] * len(words)
+    
+    while(q):
+        cur, depth = q.popleft()
+        if cur == target:
+            return depth
+        for i, word in enumerate(words):
+            if not visited[i] and can_transform(cur, word):
+                q.append((word, depth + 1))
+    
+    return 0
+                
