ba7caca069e642e1f1d1f03b036fe3d94580afaf
sene03 <hansel0311@gmail.com>
2026-05-10 11:46:57 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -5,7 +5,7 @@ with avg_score as (
     group by rest_id
 )
 select R.REST_ID, R.REST_NAME, R.FOOD_TYPE, R.FAVORITES, R.ADDRESS, A.SCORE
-from rest_info R left join avg_score A
+from rest_info R inner join avg_score A
 on R.rest_id = A.rest_id
-where R.address like '서울%' and A.score is not null
+where R.address like '서울%'
 order by A.score desc, R.favorites desc;
