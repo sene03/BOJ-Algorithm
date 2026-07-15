afd9cd0b6532b97f958e06767684acd018350350
sene03 <hansel0311@gmail.com>
2026-07-15 12:15:40 +0900

    [level 2] Title: 가장 큰 수, Time: 159.60 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,10 +1,20 @@
+from functools import cmp_to_key
+
+def comparator(a,b):
+    n1 = a+b
+    n2 = b+a
+    if n1>n2:
+        return 1
+    elif n1<n2:
+        return -1
+    else:
+        return 0
+
 def solution(numbers):
     numbers = list(map(str, numbers))
     
-    # '9' > '998' 을 만들기 위해서 '999'와 '989898'을 비교
-    numbers.sort(key=lambda x: x*3, reverse=True)
+    numbers.sort(reverse=True, key=cmp_to_key(comparator))
     
-    ans = ''.join(numbers)
+    answer = ''.join(numbers)
     
-    return '0' if ans[0] == '0' else ans
-    
+    return '0' if answer[0] == '0' else answer
